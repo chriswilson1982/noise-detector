fd562506f4da1e1d14ba87aca5814064f40c734a
Chris Wilson <c.m.wilson@me.com>
2021-02-10 10:04:47 +0000

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,19 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin python
 
 import sys
 import push
 
-THRESHOLD = 0.01 
+# Change threshold based on requirements
+THRESHOLD = 0.01
 
+# Pushover credentials (optional)
 PUSHOVER_USER_KEY = ""
 PUSHOVER_API_KEY = ""
 
+# Variables for suppressing notifications for 10 seconds after a trigger
 count = 0
 suppress = False
 
+# Main loop
 while True:
     try:
-        line = sys.stdin.readline().strip() # e.g. "0.006543"
+        # Receive audio level from stdin
+        line = sys.stdin.readline().strip()  # e.g. "0.006543"
         number = float(line)
         if number > THRESHOLD and not suppress:
             p = push.PushoverSender(PUSHOVER_USER_KEY, PUSHOVER_API_KEY)
@@ -29,16 +34,18 @@ while True:
         if count >= 5:
             count = 0
             suppress = False
-        
+            
+    # Value error
     except ValueError:
         # Cannot coerce to float or error in stdin
         print("Value error: " + line)
         break
     
+    # Keyboard interrrupt
     except KeyboardInterrupt:
         # Cancelled
         print("Baby monitor script ending")
         break
+
+# Exit
 sys.exit()
-
-
