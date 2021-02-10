82627869842bb466ff1f1fdf46e3d761438343df
Chris Wilson <c.m.wilson@me.com>
2021-02-10 09:42:42 +0000

    Update babymonitor.sh

--- babymonitor.sh
+++ babymonitor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 trap break INT
 while true; do
-    arecord --device=hw:1,0 --format S16_LE --rate 44100 -d 2 /dev/shm/tmp_rec.wav ; sox -t .wav /dev/shm/tmp_rec.wav -n stat 2>&1 | grep "RMS     amplitude" | tail -c 9
+    arecord --device=hw:1,0 --format S16_LE --rate 44100 -d 2 |
+        sox -t .wav - -n stat 2>&1 |
+        grep "RMS *amplitude" |
+        cut -d: -f2
 done
